40bc6ca2bc8efe11f6caf46ae554141dd1cdc4bc
Cobiscuit <cobainirwin@gmail.com>
2024-10-10 21:17:00 -0500

    Me and do alot, am add player... uh asteroids... uh collisions and pew pew...will research how to add scores and respawns next, or maybe cry all night long.

--- main.py
+++ main.py
@@ -1,57 +1,61 @@
+import sys
 import pygame
 from constants import *
-from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     pygame.init()
-    
-    screen =  pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    pygame.display.set_caption("Asteroids")
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+
     dt = 0
-    """
-    So there are three parts to the game loop:
-    1: Handle events
-        what is a event handler?
-    2: update game world/state
-        we do this with a for loop to cycle through all the objects in the game 
-    3: draw the game to the screen
-        once the for loop is done we then draw the game onto the screen and the for loop starts again
-    ___________
-    Setting up FPS next, or delta time, how ever you want to call it. ill limit the game to 60 fps.
-    
-    """
-    #game loop starting
-    running = True
-    while running:
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return running == False
-                
+                return
+
         for obj in updatable:
             obj.update(dt)
-                
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
-        
+
         for obj in drawable:
             obj.draw(screen)
-            
+
         pygame.display.flip()
-        
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-    
-    
